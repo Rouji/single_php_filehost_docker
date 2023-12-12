c6bd9e59d09402d063a2a522c318d1f0edd4dc34
Andreas Hackl <a@r0.at>
2023-12-13 00:17:17 +0100

    ID_LENGTH -> {MIN,MAX}_ID_LENGTH

--- entry.sh
+++ entry.sh
@@ -4,7 +4,8 @@
 [ -z "$MIN_FILEAGE" ]    && MIN_FILEAGE=10
 [ -z "$MAX_FILEAGE" ]    && MAX_FILEAGE=180
 [ -z "$UPLOAD_TIMEOUT" ] && UPLOAD_TIMEOUT=300
-[ -z "$ID_LENGTH" ]      && ID_LENGTH=3
+[ -z "$MIN_ID_LENGTH" ]  && MIN_ID_LENGTH=3
+[ -z "$MAX_ID_LENGTH" ]  && MAX_ID_LENGTH=24
 [ -z "$ADMIN_EMAIL" ]    && ADMIN_EMAIL="admin@example.com"
 [ -z "$AUTO_FILE_EXT" ]  && AUTO_FILE_EXT=false
 
@@ -17,7 +18,8 @@ sed -i "s/\(const MAX_FILESIZE\)[^;]*/\1 = ${MAX_FILESIZE}/" /srv/single_php_fil
 sed -i "s/\(const MIN_FILEAGE\)[^;]*/\1 = ${MIN_FILEAGE}/" /srv/single_php_filehost/index.php
 sed -i "s/\(const MAX_FILEAGE\)[^;]*/\1 = ${MAX_FILEAGE}/" /srv/single_php_filehost/index.php
 sed -i "s/\(const UPLOAD_TIMEOUT\)[^;]*/\1 = ${UPLOAD_TIMEOUT}/" /srv/single_php_filehost/index.php
-sed -i "s/\(const ID_LENGTH\)[^;]*/\1 = ${ID_LENGTH}/" /srv/single_php_filehost/index.php
+sed -i "s/\(const MIN_ID_LENGTH\)[^;]*/\1 = ${MIN_ID_LENGTH}/" /srv/single_php_filehost/index.php
+sed -i "s/\(const MAX_ID_LENGTH\)[^;]*/\1 = ${MAX_ID_LENGTH}/" /srv/single_php_filehost/index.php
 sed -i "s/\(const ADMIN_EMAIL\)[^;]*/\1 = \'${ADMIN_EMAIL}\'/" /srv/single_php_filehost/index.php
 sed -i "s/\(const AUTO_FILE_EXT\)[^;]*/\1 = ${AUTO_FILE_EXT}/" /srv/single_php_filehost/index.php
 sed -i "s/\(const STORE_PATH\)[^;]*/\1 = '\/files\/'/" /srv/single_php_filehost/index.php
